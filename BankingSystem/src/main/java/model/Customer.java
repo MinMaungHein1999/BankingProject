ddbb1a92c93c9aee9aba4868cc32497e77e289e9
minmaung132h <minmaung132h@prestigein.com>
2024-11-16 11:41:22 +0700

    created model

---
+++ BankingSystem/src/main/java/model/Customer.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.sql.Date;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String nrc;
+    private String phone;
+    private String address;
+    private CustomerStatus status;
+    private Date dateOfBirth;
+
+}
